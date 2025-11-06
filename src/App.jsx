3a1d49f8640dcae13e8feaf7325b39c2aac144ce
VIPUL070 <sutharvipul007@gmail.com>
2025-11-06 15:44:35 +0530

    fetching the cabins data and mutating

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { Route, Routes, Navigate } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import GlobalStyles from "./styles/GlobalStyles";
 import Dashboard from "./pages/Dashboard";
 import Account from "./pages/Account";
@@ -10,9 +12,20 @@ import Users from "./pages/Users";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./ui/AppLayout";
 
+//1. setup QueryClient
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // staleTime: 60 * 1000,
+      staleTime : 0,
+    },
+  },
+}); //2. initialize QueryClientProvider
+
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false}/>
       <GlobalStyles />
       <Routes>
         <Route element={<AppLayout />}>
@@ -27,7 +40,7 @@ function App() {
         <Route path="login" element={<Login />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
-    </>
+    </QueryClientProvider>
   );
 }
 
--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,11 +1,22 @@
 import supabase from "./supabase";
 
 export async function getCabins() {
-  let { data , error } = await supabase.from("cabins").select("*");
+  let { data, error } = await supabase.from("cabins").select("*");
 
   if (error) {
     console.log(error);
     throw new Error("Cabins could not be loaded");
+  }
+
+  return data;
+}
+
+export async function deleteCabin(id) {
+  const { data ,error } = await supabase.from("cabins").delete().eq("id", id);
+
+  if (error) {
+    console.log(error);
+    throw new Error("Cabins could not be deleted");
   }
 
   return data;
