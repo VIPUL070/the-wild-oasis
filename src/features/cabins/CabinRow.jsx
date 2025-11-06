8147d5e59cb004cb32f5d441a277b52e7a30c9be
VIPUL070 <sutharvipul007@gmail.com>
2025-11-06 16:01:03 +0530

    using toast for displaying notifications

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -2,6 +2,7 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { formatCurrency } from "../../utils/helpers";
 import { deleteCabin } from "../../services/apiCabins";
 import styled from "styled-components";
+import toast from "react-hot-toast";
 
 const TableRow = styled.div`
   display: grid;
@@ -61,10 +62,13 @@ function CabinRow({ cabin }) {
     onSuccess : () => {
        queryClient.invalidateQueries({
         queryKey : ["cabins"],
-       })
+       });
+      toast.success("Cabin deleted successfully");
     },
     
-    onError : (err) => alert(err.message)
+    onError : (err) => {
+      toast.error(err.message)
+    }
   });
 
   return (
