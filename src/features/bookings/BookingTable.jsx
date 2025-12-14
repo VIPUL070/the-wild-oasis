4360588eeb283c639f12db194ff4f2abc0a97eea
VIPUL070 <sutharvipul007@gmail.com>
2025-12-14 22:42:18 +0530

    implementing the pagination for bookings table

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -4,6 +4,7 @@ import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
 import Empty from "../../ui/Empty";
 import Spinner from "../../ui/Spinner";
+import Pagination from "../../ui/Pagination";
 
 function BookingTable() {
   const { bookings , isLoading , error } = useBookings();
@@ -36,6 +37,9 @@ function BookingTable() {
             <BookingRow key={booking.id} booking={booking} />
           )}
         />
+        <Table.Footer>
+          <Pagination count={bookings.length}/>
+        </Table.Footer>
       </Table>
     </Menus>
   );
