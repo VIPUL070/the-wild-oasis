038d70aa11c9a1fb11ca584037ae2ac379acf7d6
VIPUL070 <sutharvipul007@gmail.com>
2025-12-21 13:42:48 +0530

    Computing recent bookings and stays data for the dashboard

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings";
+import Spinner from "../../ui/Spinner";
+import { useRecentStays } from "./useRecentStays";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +9,21 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  const { bookings , isLoading : isLoadingBookings} = useRecentBookings();
+  const { stays , isLoading : isLoadingStays } = useRecentStays();
+
+  if(isLoadingBookings || isLoadingStays) return <Spinner />
+
+  return (
+    <StyledDashboardLayout>
+      <div>statistics</div>
+      <div>Todays activity</div>
+      <div>chart stay duration</div>
+      <div>char sales</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -47,11 +47,11 @@ export async function getBookings({filter , sortBy , page}){
   return {data,count };
 }
 
-// Returns all BOOKINGS that are were created after the given date. Useful to get bookings created in the last 30 days, for example.
+// Returns all BOOKINGS that are were created after the given date. Useful to get bookings created in the last 30 days, for example. //date == ISOstring
 export async function getBookingsAfterDate(date) {
   const { data, error } = await supabase
     .from("bookings")
-    .select("created_at, totalPrice, extrasPrice")
+    .select("created_at, totalPrice, extraPrice")
     .gte("created_at", date)
     .lte("created_at", getToday({ end: true }));
 
@@ -67,7 +67,6 @@ export async function getBookingsAfterDate(date) {
 export async function getStaysAfterDate(date) {
   const { data, error } = await supabase
     .from("bookings")
-    // .select('*')
     .select("*, guests(fullName)")
     .gte("startDate", date)
     .lte("startDate", getToday());
